2e545018f030c02198271af468f1df7747867e4f
funayamateppei <t.funayama0217@gmail.com>
2024-08-18 14:28:15 +0900

    check auth user

--- src/components/auth/Success.tsx
+++ src/components/auth/Success.tsx
@@ -1,3 +1,19 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "../../firebase"
+
 export function Success() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(
+      auth,
+      (user) => !user && navigate("/login")
+    )
+
+    return () => unSub()
+  }, [navigate])
+
   return <div>Success</div>
 }
