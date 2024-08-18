4f46e0e4a4e1294addc3d7accff5e9228edfaa96
funayamateppei <t.funayama0217@gmail.com>
2024-08-18 16:24:56 +0900

    chore: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import "./App.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Success, Login } from "./components"
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Success />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Success />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
