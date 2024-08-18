0350b74d71ef346eb5075b3014e5a4efbe708e02
funayamateppei <t.funayama0217@gmail.com>
2024-08-18 13:59:19 +0900

    chore: lint

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,8 @@
 //Firebase ver9 compliant (modular)
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; //追加
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage" //追加
 
 const firebaseApp = initializeApp({
   apiKey: import.meta.env.VITE_API_KEY,
@@ -11,9 +11,9 @@ const firebaseApp = initializeApp({
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-});
+})
 
 //Firebase ver9 compliant (modular)
-export const storage = getStorage(firebaseApp); //追加
-export const auth = getAuth(firebaseApp);
-export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp) //追加
+export const auth = getAuth(firebaseApp)
+export const db = getFirestore(firebaseApp)
